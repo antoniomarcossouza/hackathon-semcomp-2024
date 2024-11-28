23931f6507e0a46015d7324f2342a18d1b9ee3a4
Alexandre <alexandre@brainyit.com.br>
2024-11-27 22:25:58 -0300

    feat: add POST for rules and homepage

--- flaskr/novarumApp.py
+++ flaskr/novarumApp.py
@@ -1,6 +1,6 @@
 import os
-
 import psycopg2
+from psycopg2 import sql
 from dotenv import load_dotenv
 from flask import (
     Flask,
@@ -16,6 +16,7 @@ user = os.getenv("POSTGRES_USER")
 password = os.getenv("POSTGRES_PASSWORD")
 app.secret_key = os.getenv("APP_SECRET_KEY")
 
+print(database, user, password, app.secret_key)
 
 def get_db_connection():
     conn = psycopg2.connect(
@@ -37,22 +38,33 @@ def rules():
     conn = get_db_connection()
     cur = conn.cursor()
     if conn and cur:
-        cur.execute("SELECT * FROM regras;")
+        cur.execute("SELECT * FROM tb_regras;")
         rulelist = cur.fetchall()
     else:
         rulelist = ["Conexão com o banco falhou!"]
-    cur.close()
-    conn.close()
     if request.method == "GET":
-        return render_template("rules/rules_table.html", rulelist=rulelist)
-    # elif request.method == 'POST':
-    #     if 'rule' in request.form.keys():
-    #         rule = request.form.get('rule')
-    #         if rule != '' :
-    #             return render_template('rules/rules_submit.html')
-    #         else:
-    #             return 400
+        cur.close()
+        conn.close()
+        return render_template("rules/rules_table.html", rulelist=rulelist, queryllm='Sua query será gerada aqui')
+    elif request.method == 'POST':
+        if 'texto' in request.form.keys() and 'nome_tabela' in request.form.keys():
+            table_name = request.form.get('nome_tabela')
+            rule = request.form.get('texto')
+            action = request.form.get('action')
+            if action=='saverule':
+                insert_query = sql.SQL("INSERT INTO tb_regras (nome_tabela, texto) VALUES (%s, %s)")
+                cur.execute(insert_query, (table_name, rule))
+                conn.commit()
+                return f"{table_name} e {rule} inseridos na tabela com sucesso!", 200
 
+            elif action=='submitquery':
+                # TODO LLM
+                queryllm = rule
+                return render_template("rules/rules_table.html", rulelist=rulelist, queryllm=queryllm)
+
+        cur.close()
+        conn.close()
+        return render_template("rules/rules_table.html", rulelist=rulelist, queryllm='')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
