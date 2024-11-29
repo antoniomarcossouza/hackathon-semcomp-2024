a2396d99cee50bf378caf6fcc65a8e18f93fec71
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2024-11-28 21:51:40 -0300

    feature: redirect após criar regra

--- flaskr/novarumApp.py
+++ flaskr/novarumApp.py
@@ -48,15 +48,19 @@ def graphs():
     return render_template("rules/graphs_page.html", rulelist=rulelist)
 
 
+def get_rules(conn, cursor):
+    if conn and cursor:
+        cursor.execute("SELECT * FROM tb_regras;")
+        return cursor.fetchall()
+    return ["Conexão com o banco falhou!"]
+
+
 @app.route("/regras", methods=["GET", "POST"])
 def rules():
     conn = get_db_connection()
     cur = conn.cursor()
-    if conn and cur:
-        cur.execute("SELECT * FROM tb_regras;")
-        rulelist = cur.fetchall()
-    else:
-        rulelist = ["Conexão com o banco falhou!"]
+    rulelist = get_rules(conn, cur)
+
     if request.method == "GET":
         cur.close()
         conn.close()
@@ -80,9 +84,10 @@ def rules():
                 )
                 cur.execute(insert_query, (table_name, rule, query))
                 conn.commit()
-                return (
-                    f"{table_name} e {rule} inseridos na tabela com sucesso!",
-                    200,
+                return render_template(
+                    "rules/rules_table.html",
+                    rulelist=get_rules(conn, cur),
+                    queryllm="Sua query será gerada aqui",
                 )
             if action == "submitquery":
                 query = get_query(
@@ -91,7 +96,6 @@ def rules():
                         "rule": rule,
                     },
                 )
-
                 return render_template(
                     "rules/rules_table.html",
                     rulelist=rulelist,
